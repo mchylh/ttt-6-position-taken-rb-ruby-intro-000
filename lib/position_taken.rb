5f6c8c29e8c035a261812fe1f5d9c88ef10039b8
Michaylah Mam <mchylh@gmail.com>
2020-02-21 21:40:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,8 +2,8 @@
 board = ["", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
 def position_taken? (board, index)
-  if (board[index] == " ") || (board[index] == " ") || (board[index] == " ") 
-     (board[index] == " ") || (board[index] == " ") || (board[index] == " ") 
+  if (board[index] == " ") || (board[index] == " ") || (board[index] == " ")
+     (board[index] == " ") || (board[index] == " ") || (board[index] == " ")
      (board[index] == " ") || (board[index] == " ") || (board[index] == " ") && (board[index] == nil) || (board[index] == nil) || (board[index] == nil)
     return false
   else (board[index] == "X") || (board[index] == "O")
