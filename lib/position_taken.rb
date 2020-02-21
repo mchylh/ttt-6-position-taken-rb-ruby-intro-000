7fc3539e34682697df19896f9ea7c8c48639c175
Michaylah Mam <mchylh@gmail.com>
2020-02-21 21:04:39 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken? (board, position)
-  if (board[position] == " ") || (board[position] == " ") || (board[position] == " ")
+  if (board[position] == " ") || (board[position] == " ") || (board[position] == " ") && (board[position] == nil) || (board[position] == nil) || (board[position] == nil)
     return false
   else
     return true
