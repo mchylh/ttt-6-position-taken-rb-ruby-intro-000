7b999f3ff9aee9833b68d79e70723fd411166e9c
Michaylah Mam <mchylh@gmail.com>
2020-02-21 20:20:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,6 @@ def position_taken? (board, index)
   if (board[index] == " ") || (board[index] == " ") || (board[index] == nil)
     return false
   else
-    return true  
+    return true
   end
 end
