4e754d6a725e214d69489deebb2cd3ee45a8a90d
Michaylah Mam <mchylh@gmail.com>
2020-02-21 20:07:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
   if (board[index] == " ") || (board[index] == " ") || (board[index] == nil)
-    return false
+    return 'returns false if the board has an empty string "" in the index'
   else
     return true
   end
