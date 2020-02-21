30d44f2a6917b56e22957fa357f5877a625ac538
Michaylah Mam <mchylh@gmail.com>
2020-02-21 21:02:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,7 @@
 def position_taken? (board, position)
   if (board[position] == " ") || (board[position] == " ") || (board[position] == " ")
     return false
-    elsif (board[position] == nil) || (board[position] == nil) || (board[position] == nil)
-    return
-    else
+  else
     return true
   end
 end
