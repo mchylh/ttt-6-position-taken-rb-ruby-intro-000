7523101352ebc353c1625e8ce41c4b75bbbfc7f9
Michaylah Mam <mchylh@gmail.com>
2020-02-21 21:25:08 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,16 +1,9 @@
 # code your #position_taken? method here!
-def input_to_index(user_input)
-  user_input.to_i-1
-end
-
 def move(board, position, char=" ")
   board[position] = char
-end
-
-def position_taken? (move)
-  if (move == " ") && (move == nil)
+  if (char == " ") && (char == nil)
     return false
-  else (move == "X") || (move == "O")
+  else (char == "X") || (char == "O")
     return true
   end
 end
