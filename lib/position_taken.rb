ce653e438a5f9e1a1cc6cb64341d7f0253908a46
Michaylah Mam <mchylh@gmail.com>
2020-02-21 20:04:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,9 @@
 # code your #position_taken? method here!
-def move(board, index, player)
-  board[index] = player
-end
+def position_taken? (board, index)
+  if (board[index] == " ") || (board[index] == " ") || (board[index] == nil)
+    return false
+  else
+    return true
+  end
+end        
+    
